d9de67d9df8fae48a38254b4b93589d4d3947837
Mario Mancilla <mariormt17@gmail.com>
2021-01-11 20:28:02 -0600

    Test the secret handshake class with RSpec

---
+++ spec/secret_handshake_spec.rb
@@ -0,0 +1,50 @@
+require_relative '../src/secret_handshake'
+
+RSpec.describe SecretHandshake do
+  it 'right attributes initialization' do
+    test_handshake = SecretHandshake.new(0)
+    expect(test_handshake.code).to eq([])
+  end
+
+  describe '#code' do
+    it 'test right code with binary number 1' do
+      test_handshake = SecretHandshake.new(1)
+      expect(test_handshake.code).to eq(['wink'])
+    end
+
+    it 'test right code with binary number 10' do
+      test_handshake = SecretHandshake.new(2)
+      expect(test_handshake.code).to eq(['double blink'])
+    end
+
+    it 'test right code with binary number 100' do
+      test_handshake = SecretHandshake.new(4)
+      expect(test_handshake.code).to eq(['close your eyes'])
+    end
+
+    it 'test right code with binary number 1000' do
+      test_handshake = SecretHandshake.new(8)
+      expect(test_handshake.code).to eq(['jump'])
+    end
+
+    it 'test right code with decimal number 11' do
+      test_handshake = SecretHandshake.new(3)
+      expect(test_handshake.code).to eq(['wink', 'double blink'])
+    end
+
+    it 'test right code with decimal number 19' do
+      test_handshake = SecretHandshake.new(19)
+      expect(test_handshake.code).to eq(['double blink', 'wink'])
+    end
+
+    it 'test right code with decimal number 31' do
+      test_handshake = SecretHandshake.new(31)
+      expect(test_handshake.code).to eq(['jump', 'close your eyes', 'double blink', 'wink'])
+    end
+
+    it 'test right code with decimal number 15' do
+      test_handshake = SecretHandshake.new(15)
+      expect(test_handshake.code).to eq(['wink', 'double blink', 'close your eyes', 'jump'])
+    end
+  end
+end
--- src/secret_handshake.rb
+++ src/secret_handshake.rb
@@ -1,35 +1,32 @@
 class SecretHandshake
-    attr_reader :code
+  attr_reader :code
 
-    HANDSHAKE_ELEMENTS = {
-        1000 => 'jump',
-        100 => 'close your eyes',
-        10 => 'double blink',
-        1 => 'wink',
+  HANDSHAKE_ELEMENTS = {
+    1000 => 'jump',
+    100 => 'close your eyes',
+    10 => 'double blink',
+    1 => 'wink',
+  }
+
+  def initialize(newDecimal)
+    #Convertir el número decimal a base binaria
+    numBinary = newDecimal.to_s(2)
+    #Obtener el código en base al número ingresado
+    @code = makeCode(numBinary.to_i)
+    #Si el número en binario es mayor a 10000 invertimos el código
+    @code.reverse! if numBinary.to_i > 10000
+  end
+
+  private
+  def makeCode(numBinary)
+    code = []
+    numBinary -= 10000 if numBinary > 10000
+    HANDSHAKE_ELEMENTS.each_pair{ |key, value|
+      if(numBinary/key == 1 && numBinary != 0)
+        code << value
+        numBinary -= key
+      end
     }
-
-    def initialize(newDecimal)
-        #Convertir el número decimal a base binaria
-        numBinary = newDecimal.to_s(2)
-        #Obtener el código en base al número ingresado
-        @code = makeCode(numBinary.to_i)
-        #Si el número en binario es mayor a 10000 invertimos el código
-        @code.reverse! if numBinary.to_i > 10000
-    end
-
-    private
-    def makeCode(numBinary)
-        code = []
-        numBinary -= 10000 if numBinary > 10000
-        HANDSHAKE_ELEMENTS.each_pair{ |key, value|
-            if(numBinary/key == 1 && numBinary != 0)
-                code << [value]
-                numBinary -= key
-            end
-        }
-        code
-    end
+    code.reverse!
+  end
 end
-
-saludo = SecretHandshake.new(17)
-print saludo.code
